91df33a964a47767dbb57168d07099cee85ce497
Thomacia <thomas@thoemda.com>
2018-03-12 14:44:50 +0100

    Sequence
    Made first version of the sequence

---
+++ Assets/Scripts/SetSequence.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SetSequence : MonoBehaviour {
+
+	private List<Sprite> currentSequence = new List<Sprite>();
+	private int sequenceAmount = 3;
+    [SerializeField] private Sprite[] colors;
+    [SerializeField] private SpriteRenderer one;
+    [SerializeField] private SpriteRenderer two;
+    [SerializeField] private SpriteRenderer three;
+    private Sprite temp;
+	
+	void Update () 
+	{
+        Generate();
+	}
+
+    private void Generate()
+    {
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            for (int i = 0; i < colors.Length; i++)
+            {
+                currentSequence.Add(colors[i]);
+            }
+            GetRandom();
+            one.sprite = temp;
+
+            GetRandom();
+            two.sprite = temp;
+
+            GetRandom();
+            three.sprite = temp;
+
+            currentSequence.Clear();
+        }
+    }
+    private void GetRandom()
+    {
+        temp = currentSequence[Random.Range(0, currentSequence.Count)];
+        Debug.Log(currentSequence.Count);
+        currentSequence.Remove(temp);
+    }
+}
