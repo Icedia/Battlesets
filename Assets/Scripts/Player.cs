667e75204b4cdc76b23014efe0c05f5da1295c3f
Thomacia <thomas@thoemda.com>
2018-03-13 11:11:49 +0100

    Health_startup
    created the interface

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IHealth<float>
+{
+    private float playerHealth;
+
+	void Start ()
+    {
+        Health(100);
+
+	}
+	
+	void Update ()
+    {
+        Debug.Log(playerHealth);
+	}
+
+    public void Health(float health)
+    {
+        playerHealth = health;
+        Debug.Log(health);
+    }
+}
