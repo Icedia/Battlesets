ceab58a155081b6c7431e1b49d8d37f506571eeb
Thomacia <thomas@thoemda.com>
2018-03-13 12:00:40 +0100

    Health almost done
    for enemy and player

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Enemy : MonoBehaviour, IHealth<float>
+{
+    [SerializeField] private Slider enemyHealthBar;
+    private float enemyHealth;
+
+    void Start ()
+    {
+        Health(100);
+	}
+	
+	void Update ()
+    {
+        if (Input.GetKeyUp(KeyCode.Y))
+        {
+            DoDamage(10);
+        }
+        enemyHealthBar.value = enemyHealth;
+	}
+
+    public void Health(float health)
+    {
+        enemyHealth = health;
+    }
+
+    private void DoDamage(int damage)
+    {
+        enemyHealth -= damage;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour, IHealth<float>
 {
     private float playerHealth;
+    [SerializeField]private Slider playerHealthBar;
 
 	void Start ()
     {
@@ -15,11 +17,22 @@ public class Player : MonoBehaviour, IHealth<float>
 	void Update ()
     {
         Debug.Log(playerHealth);
-	}
+        playerHealthBar.value = playerHealth;
+        if (Input.GetKeyUp(KeyCode.T))
+        {
+            DoDamage(10);
+        }
+
+    }
 
     public void Health(float health)
     {
         playerHealth = health;
         Debug.Log(health);
+    }
+
+    private void DoDamage(int damage)
+    {
+        playerHealth -= damage;
     }
 }
