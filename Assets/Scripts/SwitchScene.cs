65061df8324d8ed7b92a8d425de9eaedf2c59153
Thomacia <thomas@thoemda.com>
2018-03-21 12:07:36 +0100

    SwitchScene_implemented

---
+++ Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    [SerializeField] private string Destination;
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene(Destination);
+    }
+
+}
